eef7dd83b484c5f7cb88ae868d20e4d8dca8c482
Pedro Almeida <100890704+Brownlius@users.noreply.github.com>
2023-03-31 17:06:53 -0400

    NextJs Inicio

--- src/app/page.js
+++ src/app/page.js
@@ -1,90 +1,36 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+    <main className="flex box-border items-center flex-center py-[2%] pl-[5%] pr-[1%] bg-slate-950 h-[100vh] w-full">
+      <div className="flex flex-center flex-col w-[45%] p-2">
+        <div className='p-2'><Image
+          src="alura.svg" width="200" height="100" alt='Logo Alura' />
+        </div>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h1 className='font-bold text-center text-6xl py-2'>Alura Cases</h1>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div className="flex flex-row flex-1 gap-1 text-center items-center justify-start" >
+          <p className="text-sm font-semibold bg-violet-500 p-1 rounded-xl">O que tem de novo?</p>
+          <p className="text-sm font-semibold">Confira as principais dúvidas <b className='text-violet-500'>&gt;</b></p>
         </div>
+        <div className=''>
+          <p className='p-2'>O Alura Cases é uma plataforma online que conecta empresas que buscam soluções personalizadas de negócios com estudantes e profissionais de tecnologia capacitados. A plataforma oferece diversos desafios em áreas como desenvolvimento de software, design, marketing digital e análise de dados. As empresas cadastradas podem criar briefings detalhados para cada desafio, enquanto os estudantes e profissionais têm a oportunidade de aplicar seus conhecimentos em projetos reais e aprender com a experiência. A plataforma também oferece uma comunidade ativa de alunos e profissionais que compartilham conhecimentos e experiências, além de suporte especializado.</p>
+          <p className='text-center text-xl font-semibold'>
+            Quer testar antes de todo mundo?
+          </p>
+        </div>
+        <form className='w-3/4 flex flex-1 gap-2 justify-between m-auto p-2'>
+          <input className=' text-slate-950 w-2/3 rounded-xl p-2 text-md bg-slate-300' placeholder='Insira seu e-mail' />
+          <button className='bg-violet-500 font-bold rounded-xl p-2 text-md w-1/3'>Cadastrar</button>
+
+        </form>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className=' absolute -right-1/4'>
+        <Image className='   rounded-xl shadow-md shadow-black' src="/alura-cases.png" width="800" height="380" priority alt='Site do alura cases' />
       </div>
     </main>
   )
